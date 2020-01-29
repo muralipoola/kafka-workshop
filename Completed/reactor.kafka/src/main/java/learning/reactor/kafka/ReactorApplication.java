01f301632bad6a0d6cf1646eb59239faae8562df
Murali Poola <muralipa@thoughtworks.com>
2020-01-29 21:21:05 +0530

    Added reactor.kafka module

---
+++ Completed/reactor.kafka/src/main/java/learning/reactor/kafka/ReactorApplication.java
@@ -0,0 +1,13 @@
+package learning.reactor.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReactorApplication.class, args);
+  }
+
+}
